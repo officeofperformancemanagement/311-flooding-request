5982aca9a4ca9e22ef861d8465f14412178a94a2
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-27 13:21:28 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl --output "data.json" "https://vc0.publicstuff.com/api/2.0/requests_list?client_id=1000051&limit=1000&request_type_id=1004912&after=1727395200"
+curl --output "data.json" "https://vc0.publicstuff.com/api/2.0/requests_list?client_id=1000051&limit=30&request_type_id=1004912&after=1727395200"
